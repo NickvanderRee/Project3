3bf5031e4c4d4b84d6aa4177014abd861da18bb5
NickvanderRee <112868944+NickvanderRee@users.noreply.github.com>
2023-01-30 14:02:55 +0100

    Add files via upload

---
+++ project3/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My First HTML Page</title>
+  </head>
+  <body>
+    <header>
+    </header>
+    <main>
+      <h1>Welcome to my website</h1>
+    <?php 
+      echo "Today's date is: " . date("Y-m-d");
+    ?>
+    </main>
+    <footer>
+    </footer>
+  </body>
+</html>
